cf3e33b91222ddae9bb584f254a549b8ec8419a1
AbhayPatil <abhay.patil@students.iiit.ac.in>
2022-01-28 01:48:38 +0530

    updated file

--- build/CMakeFiles/app.dir/cmake_clean.cmake
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -1,4 +1,6 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/src/enemy.cpp.o"
+  "CMakeFiles/app.dir/src/enemy.cpp.o.d"
   "CMakeFiles/app.dir/src/main.cpp.o"
   "CMakeFiles/app.dir/src/main.cpp.o.d"
   "app"
---
+++ build/CMakeFiles/app.dir/src/enemy.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/app.dir/src/enemy.cpp.o: \
+ /home/abhay/Desktop/2-2/CG/A1/Game/src/enemy.cpp \
+ /usr/include/stdc-predef.h
